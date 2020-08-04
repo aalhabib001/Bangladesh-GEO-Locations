c2cc5490f3d85bb81edfb594b5fc578e07aa4fb9
Habib <aalhabib001@gmail.com>
2020-08-05 05:14:45 +0600

    first commit

---
+++ Division/Division-Pgsql.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS "Division" (
+"id" INT NULL,
+"name" TEXT NULL
+);
+
+INSERT INTO Division VALUES
+(1,'Chattagram'),
+(2,'Rajshahi'),
+(3,'Khulna'),
+(4,'Barisal'),
+(5,'Sylhet'),
+(6,'Dhaka'),
+(7,'Rangpur'),
+(8,'Mymensingh');
